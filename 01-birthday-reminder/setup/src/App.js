4cb2beebabfc0e527e679cfba3be95c5df4694e9
Swati Sinha <88704889+Swati213@users.noreply.github.com>
2021-08-31 02:14:32 +0530

    Add files via upload

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+cont [der,setder]=useState();
+function App() {
+
+  return <h2>reminder project setup</h2>;
+}
+
+export default App;
