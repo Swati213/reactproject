5032685dacd6675e5cf0942ba671d09bdc484446
Swati Sinha <88704889+Swati213@users.noreply.github.com>
2021-08-31 02:19:07 +0530

    Add files via upload

---
+++ forms/src/api.js
@@ -0,0 +1,27 @@
+export const questions=[
+    {
+        id:1,
+        question:'What is the Virtual DOM?',
+        answer:'The Virtual DOM is an abstraction of the HTML DOM.'
+        
+    },
+    {
+        id:2,
+        question:'How do you create a react app?',
+        answer:' installed create-react-app globally via npm install -g create-react-app'
+        
+    },{
+        id:3,
+        question:'what is event inReact?',
+        answer:' An event is an action that could be triggered as a result of the user action or system generated event.'
+        
+    },{
+        id:4,
+        question:'What are synthetic events in React?',
+        answer:'Synthetic events are the objects which act as a cross-browser wrapper around the browser’s native event.'
+    },{
+        id:5,
+        question:'What are components in React?',
+        answer:'Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML via a render() function. Components come in two types, Class components and Function components, in this tutorial we will concentrate on Class components'
+    }
+]
